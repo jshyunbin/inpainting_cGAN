ba0bbc9fc43ce42ee254533e224a06e12029cb7b
Hyunbin Lee <46880076+jshyunbin@users.noreply.github.com>
2019-06-05 15:22:37 +0900

    simple commit

--- semantic-image-inpainting-master/src/dcgan.py
+++ semantic-image-inpainting-master/src/dcgan.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.contrib.layers import flatten
 import matplotlib as mpl
-mpl.use('TkAgg')  # or whatever other backend that you want to solve Segmentation fault (core dumped)
+# mpl.use('TkAgg')  # or whatever other backend that you want to solve Segmentation fault (core dumped)
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
